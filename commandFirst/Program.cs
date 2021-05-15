ff076c5a5b00365ce682099909282156463e98ab
ridibundus <61305007+ridibundus@users.noreply.github.com>
2021-05-15 10:51:05 +0900

    とりま

--- commandFirst/Program.cs
+++ commandFirst/Program.cs
@@ -1,30 +1,76 @@
 ﻿using System;
 using System.Data;
-using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
 
 namespace CommandFirst
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            string str = @"Data Source=LocalHost;Initial Catalog=biginPre;User ID=sa;Password=P@ssw0rd;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False";
-            //string str = "Data Source=LAPTOP-VSS7FKD8;Initial Catalog=biginPre;user id=sal;password=P@ssw0rd";
-            //+ "Integrated Security=SSPI";
-            string qs = "SELECT * FROM firstStep;";
-            CreateCommand(qs, str);
+            string connectionString = @"Data Source=localhost;Initial Catalog=biginPre;User ID=sa;Password=P@ssw0rd;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False";
+
+            Reader(@"Select * from firstStep;", connectionString);
+            Console.WriteLine("insert!");
+            Execute(@"Insert into firstStep([key],value) VALUES('A','alpha')", connectionString);
+            ParameterizedCreate("B", "bravo", connectionString);
+            Reader(@"Select * from firstStep;", connectionString);
+
+
         }
-        private static void CreateCommand(string queryString,
-            string connectionString)
+        private static void Reader(string queryString, string connectionString)
         {
-            using (SqlConnection connection = new SqlConnection(
-                connectionString))
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 SqlCommand command = new SqlCommand(queryString, connection);
-                
                 command.Connection.Open();
-                command.ExecuteNonQuery();
+
+                using SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read()) Console.WriteLine($"{reader[0]}, {reader[1]}, {reader[2]}");
             }
         }
+
+        private static int Execute(string queryString, string connectionString)
+        {
+            using SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Connection.Open();
+            return command.ExecuteNonQuery();
+        }
+
+        private static int ParameterizedCreate(string key, string value, string connectionString)
+        {
+            using SqlConnection connection = new SqlConnection(connectionString);
+            string createSql = @"Insert into firstStep([key],value) VALUES(@KEY,@VALUE)";
+            SqlCommand command = new SqlCommand(createSql, connection);
+            command.Connection.Open();
+            command.Parameters.Add("@KEY", SqlDbType.NChar, 10);
+            command.Parameters.Add("@VALUE", SqlDbType.NChar, 10);
+            command.Parameters["@KEY"].Value = key;
+            command.Parameters["@VALUE"].Value = value;
+
+            return command.ExecuteNonQuery();
+        }
+
+        //static void Main()
+        //{
+        //    string str = @"Data Source=LocalHost;Initial Catalog=biginPre;User ID=sa;Password=P@ssw0rd;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False";
+        //    //string str = "Data Source=LAPTOP-VSS7FKD8;Initial Catalog=biginPre;user id=sal;password=P@ssw0rd";
+        //    //+ "Integrated Security=SSPI";
+        //    string qs = "SELECT * FROM firstStep;";
+        //    CreateCommand(qs, str);
+        //}
+        //private static void CreateCommand(string queryString,
+        //    string connectionString)
+        //{
+        //    using (SqlConnection connection = new SqlConnection(
+        //        connectionString))
+        //    {
+        //        SqlCommand command = new SqlCommand(queryString, connection);
+
+        //        command.Connection.Open();
+        //        command.ExecuteNonQuery();
+        //    }
+        //}
     }
 }
--- sqlBasicPre/Program.cs
+++ sqlBasicPre/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.Intrinsics;
+using sqlBasicPre.DataAccess.DataAccessModels;
 using sqlBasicPre.DataAccess.DbContexts;
 
 namespace sqlBasicPre
@@ -10,12 +12,71 @@ namespace sqlBasicPre
         {
             Console.WriteLine("Hello World!");
 
+            Selected();
+
+        }
+
+        private static void Selected()
+        {
+
             MyDbContext myDbContext = new MyDbContext();
 
-            string[] inn = {"aaa", "bbb", "ccc"};
+            var where = myDbContext.FirstSteps.Find(1);
 
-            //var v = myDbContext.FirstSteps.Where(s=>inn.Contains(s.Key)).ToArray();
-            var v = myDbContext.FirstSteps.ToArray();
+            Console.WriteLine($"{where.Id},{where.Key},{where.Value}");
+
+            string[] inn = { "apple", "A", "B" };
+
+            var all = myDbContext.FirstSteps;
+
+            Console.WriteLine("all");
+            foreach (FirstStep firstStep in all)
+            {
+                Console.WriteLine($"{firstStep.Id},{firstStep.Key},{firstStep.Value}");
+            }
+
+            Console.WriteLine("find");
+
+
+
+
+            var contain = myDbContext.FirstSteps.Where(s => inn.Contains(s.Key));
+
+            foreach (FirstStep firstStep in contain)
+            {
+                Console.WriteLine($"{firstStep.Id},{firstStep.Key},{firstStep.Value}");
+            }
+
+            var contain2 = myDbContext.FirstSteps.Where(s => inn.Contains(s.Key));
+
+            var v = contain2.GetEnumerator();
+
+            while (v.MoveNext())
+            {
+                var firstStep = v.Current;
+                Console.WriteLine($"{firstStep?.Id},{firstStep?.Key},{firstStep?.Value}");
+            }
+
+            var v1 = myDbContext.FirstSteps;
+            var v2 = v1.Where(s => inn.Contains(s.Key));
+
+            foreach (FirstStep firstStep in v2)
+            {
+                Console.WriteLine($"{firstStep.Id},{firstStep.Key},{firstStep.Value}");
+            }
+
+            var v3 = v2.Where(f => f.Key == "A");
+
+            foreach (FirstStep firstStep in contain)
+            {
+                Console.WriteLine($"{firstStep.Id},{firstStep.Key},{firstStep.Value}");
+            }
+
+        }
+
+        private static void FromSql()
+        {
+
         }
     }
 }
